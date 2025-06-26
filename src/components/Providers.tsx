62e5caf3df2a4f88cdb44489158943b43e7b4b3b
Enoch <arhyelphilip024@gmail.com>
2025-06-26 01:31:38 +0100

    setting amplitude analytics

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,17 +1,22 @@
 "use client";
 
+import { useEffect } from "react";
 import { PrivyProvider } from "@privy-io/react-auth";
-import * as amplitude from '@amplitude/analytics-browser';
-import { sessionReplayPlugin } from '@amplitude/plugin-session-replay-browser';
+import * as amplitude from "@amplitude/analytics-browser";
+import { sessionReplayPlugin } from "@amplitude/plugin-session-replay-browser";
 
 const privyAppId = process.env.NEXT_PUBLIC_PRIVY_APP_ID!;
 const privyClietId = process.env.NEXT_PUBLIC_PRIVY_WEB_CLIENT_ID!;
 const amplitudeApiKey = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!;
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-
-  amplitude.add(sessionReplayPlugin({sampleRate: 1}));
-  amplitude.init(amplitudeApiKey, {"fetchRemoteConfig":true, "autocapture":true});
+  useEffect(() => {
+    amplitude.add(sessionReplayPlugin({ sampleRate: 1 }));
+    amplitude.init(amplitudeApiKey, {
+      fetchRemoteConfig: true,
+      autocapture: true,
+    });
+  }, []);
 
   return (
     <PrivyProvider
